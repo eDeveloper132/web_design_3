ad6a7bb15c4948c0b11220cd8e74f35e0e345ca6
eDeveloper132 <1375219.aptechiic@gmail.com>
2024-12-12 18:01:14 +0500

    little change

--- src/app/Main/page.tsx
+++ src/app/Main/page.tsx
@@ -43,7 +43,7 @@ import pen_nib from "../../../public/pen_nib.png";
 import calendar_iop from "../../../public/calendar_iop.png";
 export default function MainHome(){
     return(
-<main className="w-full bg-white mb-28">
+<main className="w-full bg-white">
     <div className="bg-[#F2F0FF] flex flex-wrap">
         <div className="w-full sm:w-1/2 md:w-1/2 lg:w-1/4 xl:w-1/4 2xl:w-1/4">
             <Image src={lamb} alt="lamb" />
@@ -405,7 +405,7 @@ export default function MainHome(){
   </h1>
 </div>
 
-<div className="flex justify-center px-4 md:px-8">
+<div className="flex justify-center px-4 md:px-8 mb-28">
   <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-10 max-w-[1200px]">
     <Blog_Card 
       title="Top Essential Trends in 2021" 
