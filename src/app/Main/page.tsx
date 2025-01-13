1e41396477f489d21e276d1b1de88a431c0d2384
eDeveloper132 <1375219.aptechiic@gmail.com>
2025-01-13 13:33:49 +0500

    some change

--- src/app/Main/page.tsx
+++ src/app/Main/page.tsx
@@ -405,7 +405,7 @@ export default function MainHome(){
   </h1>
 </div>
 
-<div className="flex justify-center px-4 md:px-8 mb-28">
+<div className="flex justify-center px-4 md:px-8 pb-28">
   <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-10 max-w-[1200px]">
     <Blog_Card 
       title="Top Essential Trends in 2021" 
